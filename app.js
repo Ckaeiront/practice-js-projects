d165d2d36d7320a29b9985515135c7686c225fb9
Caio Programas <109165311+Ckaeiront@users.noreply.github.com>
2023-04-25 21:18:05 -0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btnColor = document.querySelector('.btn-hero');
-const backgroundColor = document.querySelector('.color');
-
-btnColor.addEventListener('click', () => {
-    const colored = colors[Math.floor(Math.random() * colors.length)];
-    document.body.style.backgroundColor = colored;
-    backgroundColor.textContent = colored;
-});
